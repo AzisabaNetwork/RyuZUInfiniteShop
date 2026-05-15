f8d1dba06f8a1cdd09ab41cb8b2b0a60e2be21ff
sysnote8main <sysnote8main@gmail.com>
2026-05-16 00:39:40 +0900

    chore: update commandlib internal spigot version to 1.20.1

--- commandlib/build.gradle.kts
+++ commandlib/build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
     implementation("org.jetbrains:annotations:26.1.0")
 }
