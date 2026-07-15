85b35f8a3fe7a6221a7c63d47cca812e786c8de2
albardoo02 <akshs@outlook.jp>
2026-07-15 23:41:33 +0900

    fix: add `else if` to prevent unintended shop initialization order

--- plugin/src/main/java/ryuzuinfiniteshop/ryuzuinfiniteshop/util/inventory/ShopUtil.java
+++ plugin/src/main/java/ryuzuinfiniteshop/ryuzuinfiniteshop/util/inventory/ShopUtil.java
@@ -87,7 +87,7 @@ public class ShopUtil {
                 String citizen = config.getString("Npc.Options.Citizen");
                 if (mythicmob != null)
                     new Shop(location, mythicmob);
-                if (citizen != null)
+                else if (citizen != null)
                     new Shop(location, UUID.fromString(citizen), false);
                 else
                     createNewShop(location, type, null);
@@ -136,7 +136,7 @@ public class ShopUtil {
         String citizen = config.getString("Npc.Options.Citizen");
         if (mythicmob != null)
             return new Shop(location, mythicmob);
-        if (citizen != null)
+        else if (citizen != null)
             return new Shop(location, UUID.fromString(citizen), false);
         else
             return createNewShop(location, type, null);
